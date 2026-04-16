5e666c4cc88e7830c4eb4382320f838827586823
Karen <techshift86@gmail.com>
2026-04-16 12:50:54 +0400

    jjjjjjj

--- components/banners/subscribe-banner/SubscribeBanner.tsx
+++ components/banners/subscribe-banner/SubscribeBanner.tsx
@@ -8,6 +8,7 @@ import useSendEmail from "@/hooks/useSendEmail";
 
 const siteUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
+
 const SubscribeBanner = () => {
   const [state, setState] = useState({
     email: "",
